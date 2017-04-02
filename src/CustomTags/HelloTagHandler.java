19160ddc7aa72096e0eb1b01e86b02e7217c0723
Denghejun <hjdeng@CNhjdeng.local>
2017-04-02 20:31:42 +0800

    java custom tag

---
+++ src/CustomTags/HelloTagHandler.java
@@ -0,0 +1,56 @@
+package CustomTags;
+
+import com.sun.javafx.scene.traversal.ContainerTabOrder;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.JspFragment;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+import java.io.StringWriter;
+
+/**
+ * Created by hjdeng on 4/1/17.
+ */
+public class HelloTagHandler extends SimpleTagSupport {
+
+    private String who;
+
+    private String said;
+
+    public void setWho(String who) {
+        this.who = who;
+    }
+
+    public void setSaid(String said) {
+        this.said = said;
+    }
+
+    private String getBody() throws IOException, JspException {
+        StringWriter bodyWriter = new StringWriter();
+
+        try {
+            this.getJspBody().invoke(bodyWriter);
+            bodyWriter.close();
+            return bodyWriter.toString();
+        } finally {
+            bodyWriter.close();
+        }
+
+    }
+
+    private String createResponse() throws IOException, JspException {
+        String response = "%s said: %s. But the point is %s";
+        return String.format(response, this.who, this.said, this.getBody());
+    }
+
+    private void Execute() throws IOException, JspException {
+        String response = this.createResponse();
+        this.getJspContext().getOut().println(response);
+    }
+
+    @Override
+    public void doTag() throws IOException, JspException {
+        this.Execute();
+    }
+}
+
---
+++ web/index.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hjdeng
+  Date: 4/1/17
+  Time: 8:10 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ taglib prefix="my" uri="http://www.thoughtworks.com" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Java Tag</title>
+</head>
+<body>
+
+<my:hello who="Hejun" said="I can do this">
+    I have no idea form now on.
+</my:hello>
+
+</body>
+</html>
